b1489be650733281918481647646cc52463a25ec
vm123 <896557187@qq.com>
2017-07-12 04:57:34 -0700

    xsp

--- c03/calculator.c
+++ c03/calculator.c
@@ -3,7 +3,14 @@ int main()
 {
 		int a=0,b=0,c=0,d=1;
 		char key='\0';
+		char user='\0';
 		printf("welcom to the shangqian student xsp's calculator\nif you want exit,please do 'ctrl'+'c'\nhave great time and great life\n");
+		printf("you want to known how to use the tool?\ny or n?");
+		scanf("%c",&user);
+		if(user=='y')
+	 
+				printf("+mean add;\n-mean minus;\n*mean multiply;\n/mean divide\nfor example num+num,num-num,num*num,num/num\nnow here we go:\n");
+
 		while(1)
 		{
 		scanf("%d%c%d",&a,&key,&b);
